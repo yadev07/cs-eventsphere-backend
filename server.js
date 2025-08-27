133487e6487134ed5a22436f4693f5796b524aad
yadev07 <yadevsingh.nishad@gmail.com>
2025-08-27 16:56:22 +0530

    Files updated

--- server.js
+++ server.js
@@ -9,7 +9,10 @@ const app = express();
 
 // Middleware
 app.use(helmet());
-app.use(cors());
+app.use(cors({
+  origin: ['https://cs-eventsphere.netlify.app','http://localhost:5001'],
+  credentials:true
+}));
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true }));
 
